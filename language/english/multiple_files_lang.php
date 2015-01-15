bcfb8e1ea158987b2773522c1fb197a5defb537f
jhubb81 <john@jhubb.co.uk>
2015-01-15 15:54:33 +0000

    Update multiple_files_lang.php
    Added English languages

--- language/english/multiple_files_lang.php
+++ language/english/multiple_files_lang.php
@@ -2,11 +2,11 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['streams:multiple_files.name'] = 'Múltiples archivos';
-$lang['streams:multiple_files.choose_stream'] = 'Stream de Relación';
-$lang['streams:multiple_files.doesnt_exist'] = 'Stream relacionado no existe.';
-$lang['streams:multiple_files.select_files'] = 'Seleccionar archivos';
-$lang['streams:multiple_files.help_draganddrop'] = 'Arrastre los archivos acá o de clic para seleccionarlos.';
-$lang['streams:multiple_files.drop_images_here'] = 'Deje los archivos acá.';
-$lang['streams:multiple_files.max_limit_error'] = 'No se guardaron todos los archivos porque se ha superado el límite permitido (%s).';
-$lang['streams:multiple_files.adding_error'] = 'No se pudo agregar el archivo, verifique que la extensión, las dimensiones y el tamaño sean los correctos.';
+$lang['streams:multiple_files.name'] = 'Multiple Files';
+$lang['streams:multiple_files.choose_stream'] = 'Choose Stream';
+$lang['streams:multiple_files.doesnt_exist'] = 'Stream doesn\'t exist';
+$lang['streams:multiple_files.select_files'] = 'Select files';
+$lang['streams:multiple_files.help_draganddrop'] = 'Drag files here or click here to select.';
+$lang['streams:multiple_files.drop_images_here'] = 'Drag files here.';
+$lang['streams:multiple_files.max_limit_error'] = 'All files were not saved because they exceed the maximum upload limit (%s).';
+$lang['streams:multiple_files.adding_error'] = 'Could not add the file! Verify that the extension, dimensions and size are correct.';
